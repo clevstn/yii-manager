e51c047a4531b5f17328c86cf62f23c71a7cb71f
yanghuilei <yang_hui_lei@163.com>
2020-09-22 20:03:16 +0800

    ...

--- builder/table/CustomControl.php
+++ builder/table/CustomControl.php
@@ -18,4 +18,20 @@ interface CustomControl
      * @since 1.0
      */
     public function render();
+
+    /**
+     * 返回用于获取值的Js脚本
+     * @return mixed
+     * @author cleverstone <yang_hui_lei@163.com>
+     * @since 1.0
+     */
+    public function getJsValues();
+
+    /**
+     * 返回用于初始化值的Js脚本
+     * @return mixed
+     * @author cleverstone <yang_hui_lei@163.com>
+     * @since 1.0
+     */
+    public function initJsValues();
 }
