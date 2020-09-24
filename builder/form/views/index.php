cc78b79be72c0d4aab1c8cba047e0ceee6d153ed
yanghuilei <yang_hui_lei@163.com>
2020-09-24 16:53:28 +0800

    devlopment

--- builder/form/views/index.php
+++ builder/form/views/index.php
@@ -10,5 +10,33 @@
 
 <!--表单内容-->
 <div class="panel-body border-bottom">
-    cccc
+    <form>
+        <div class="form-group">
+            <div class="input-group">
+                <div class="input-group-addon">
+                    <span class="w-130">邮箱</span>
+                </div>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="input-group">
+                <div class="input-group-addon">
+                    <span class="w-130">密码</span>
+                </div>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputFile">File input</label>
+            <input type="file" id="exampleInputFile">
+            <p class="help-block">Example block-level help text here.</p>
+        </div>
+        <div class="checkbox">
+            <label>
+                <input type="checkbox"> Check me out
+            </label>
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+    </form>
 </div>
