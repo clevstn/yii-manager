441e024b37712a2d79104188c72ea194fe6c4172
yanghuilei <yang_hui_lei@163.com>
2020-09-24 19:21:38 +0800

    devlopment

--- builder/form/views/index.php
+++ builder/form/views/index.php
@@ -7,15 +7,23 @@ WangEditorAsset::register($this);
 ?>
 <!--页面标题-->
 <?php if(!empty($this->title)): ?>
-    <div class="panel-heading border-bottom">
-        <span class="f-13"><?= $this->title ?></span>
+    <div class="panel-heading border-bottom clearfix">
+        <span class="f-13 pull-left">
+            <?= $this->title ?>
+        </span>
+        <div class="pull-right">
+            <a class="form-header-btn" type="button" href="#">
+                <i class="glyphicon glyphicon-arrow-left"></i>
+                <span>返回</span>
+            </a>
+        </div>
     </div>
 <?php endif; ?>
 
 <!--表单内容-->
 <div class="panel-body border-bottom">
-    <form>
-        <div class="form-group">
+    <form class="row">
+        <div class="form-group col-md-6">
             <div class="input-group">
                 <div class="input-group-addon">
                     <span class="addon-fix">邮箱</span>
@@ -23,7 +31,7 @@ WangEditorAsset::register($this);
                 <input type="email" class="form-control" id="exampleInputEmail1" placeholder="请输入邮箱">
             </div>
         </div>
-        <div class="form-group">
+        <div class="form-group col-md-6">
             <div class="input-group">
                 <div class="input-group-addon">
                     <span class="addon-fix">密码</span>
@@ -31,7 +39,7 @@ WangEditorAsset::register($this);
                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="请输入密码">
             </div>
         </div>
-        <div class="form-group">
+        <div class="form-group col-md-6">
             <div class="input-group">
                 <div class="input-group-addon text-left">
                     <span class="addon-fix text-center">文件上传</span>
@@ -39,7 +47,7 @@ WangEditorAsset::register($this);
                 <input type="file" class="form-control" id="exampleInputFile">
             </div>
         </div>
-        <div class="form-group">
+        <div class="form-group col-md-6">
             <div class="input-group">
                 <div class="input-group-addon">
                     <span class="addon-fix">文本域</span>
@@ -47,7 +55,7 @@ WangEditorAsset::register($this);
                 <textarea name="" id="" cols="30" rows="10" class="form-control" placeholder="请输入文本"></textarea>
             </div>
         </div>
-        <div class="form-group">
+        <div class="form-group col-md-6">
             <div class="input-group">
                 <div class="input-group-addon">
                     <span class="addon-fix">单选选择控件</span>
@@ -64,7 +72,7 @@ WangEditorAsset::register($this);
                 </div>
             </div>
         </div>
-        <div class="form-group">
+        <div class="form-group col-md-6">
             <div class="input-group">
                 <div class="input-group-addon">
                     <span class="addon-fix">多选选择控件</span>
@@ -81,7 +89,7 @@ WangEditorAsset::register($this);
                 </div>
             </div>
         </div>
-        <div class="form-group">
+        <div class="form-group col-md-6">
             <div class="input-group">
                 <div class="input-group-addon">
                     <span class="addon-fix">富文本1</span>
@@ -89,7 +97,7 @@ WangEditorAsset::register($this);
                 <div class="YmWangEditor"></div>
             </div>
         </div>
-        <div class="form-group">
+        <div class="form-group col-md-6">
             <div class="input-group">
                 <div class="input-group-addon">
                     <span class="addon-fix">富文本2</span>
@@ -97,6 +105,10 @@ WangEditorAsset::register($this);
                 <div class="YmWangEditor"></div>
             </div>
         </div>
-        <button type="submit" class="btn btn-default">立即提交</button>
+        <div class="form-group col-md-6">
+            <div class="addon-fix"></div>
+            <button type="button" class="btn btn-sm btn-default">重置</button>
+            <button type="button" class="btn btn-sm btn-primary">立即提交</button>
+        </div>
     </form>
 </div>
