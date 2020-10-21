4160b7eb17917f99c4c6713dc14c63cf196299db
yanghuilei <yang_hui_lei@163.com>
2020-10-21 14:53:42 +0800

    the development

--- admin/controllers/AreaCodeController.php
+++ admin/controllers/AreaCodeController.php
@@ -3,6 +3,9 @@
 namespace app\admin\controllers;
 
 use app\builder\common\CommonController;
+use app\builder\table\ToolbarFilterOptions;
+use app\builder\ViewBuilder;
+use app\models\AreaCode;
 
 /**
  * 手机区号
@@ -15,7 +18,11 @@ class AreaCodeController extends CommonController
      * {@inheritdoc}
      */
     public $actionVerbs = [
-        'index' => ['get'],
+        'index'     => ['get'],
+        'add'       => ['get', 'post'],
+        'edit'      => ['get', 'post'],
+        'toggle'    => ['get', 'post'],
+        'delete'    => ['get', 'post'],
     ];
 
     /**
@@ -23,14 +30,16 @@ class AreaCodeController extends CommonController
      */
     public $guestActions = [
         'index',
+        'add',
+        'edit',
+        'toggle',
+        'delete',
     ];
 
     /**
      * {@inheritdoc}
      */
-    public $undetectedActions = [
-        'index',
-    ];
+    public $undetectedActions = [];
 
     /**
      * 数据列表
@@ -40,6 +49,121 @@ class AreaCodeController extends CommonController
      */
     public function actionIndex()
     {
-        return '';
+        $table = ViewBuilder::table();
+        // 表格列
+        $table->columns = [
+            'name' => table_column_helper('地域名', ['style' => ['min-width' => '150px']]),
+            'code' => table_column_helper('电话区号', ['style' => ['min-width' => '150px']]),
+            'status_label' => table_column_helper('电话区号', ['style' => ['min-width' => '120px']], function ($item) {
+                return AreaCode::statusLabel($item['status']);
+            }),
+            'created_at' => table_column_helper('创建时间', ['style' => ['min-width' => '180px']]),
+            'updated_at' => table_column_helper('更新时间', ['style' => ['min-width' => '180px']]),
+        ];
+        // Sql
+        $table->query = function () {
+            $queryParams = $this->get;
+            $query = AreaCode::query(['id', 'name', 'code', 'status', 'created_at', 'updated_at']);
+            if (!empty($queryParams)) {
+                $query->filterWhere([
+                    'and',
+                    ['like', 'name', isset($queryParams['name']) ? $queryParams['name'] : null],
+                    ['like', 'code', isset($queryParams['code']) ? $queryParams['code'] : null],
+                ]);
+            }
+
+            return $query;
+        };
+
+        // 行操作
+        $table->rowActions = [
+            // 删除
+            table_action_helper('ajax', [
+                'title' => '删除',
+                'icon' => 'glyphicon glyphicon-remove',
+                'route' => 'admin/area-code/delete',
+                'method' => 'post',
+            ]),
+            // 编辑
+            table_action_helper('modal', [
+                'title' => '编辑',
+                'icon' => 'fa fa-pencil-square-o',
+                'route' => 'admin/area-code/edit',
+                'width' => '700px',
+                'height' => '500px',
+            ]),
+        ];
+
+        // 工具栏 - 刷新
+        $table->toolbarRefresh = [];
+
+        // 工具栏 - 筛选
+        $table->toolbarFilter = [
+            'columns' => [
+                'name' => table_toolbar_filter_helper([
+                    'control' => ToolbarFilterOptions::CONTROL_TEXT,
+                    'label' => '地域名',
+                    'placeholder' => '请输入地域名',
+                ]),
+                'code' => table_toolbar_filter_helper([
+                    'control' => ToolbarFilterOptions::CONTROL_TEXT,
+                    'label' => '电话区号',
+                    'placeholder' => '请输入电话区号',
+                ]),
+            ],
+        ];
+
+        // 工具栏 - 自定义
+        $table->toolbarCustom = [
+            table_toolbar_custom_helper('left', [
+                'title' => '新增',
+                'icon' => 'glyphicon glyphicon-plus',
+                'option' => 'modal',
+                'width' => '700px',
+                'height' => '500px',
+                'route' => 'admin/area-code/add',
+            ]),
+            table_toolbar_custom_helper('left', [
+                'title' => '启用',
+                'icon' => 'glyphicon glyphicon-ok-circle',
+                'option' => 'ajax',
+                'method' => 'POST',
+                'route' => 'admin/area-code/toggle',
+                'params' => ['id', 'action' => 'enabled'],
+            ]),
+            table_toolbar_custom_helper('left', [
+                'title' => '禁用',
+                'icon' => 'glyphicon glyphicon-ban-circle',
+                'option' => 'ajax',
+                'method' => 'POST',
+                'route' => 'admin/area-code/toggle',
+                'params' => ['id', 'action' => 'disabled'],
+            ]),
+        ];
+
+        // Order by
+        $table->orderBy = ['id' => SORT_DESC];
+
+        return $table->render($this);
+    }
+
+    public function actionAdd()
+    {
+
+    }
+
+    public function actionEdit()
+    {
+
+    }
+
+    public function actionToggle()
+    {
+
+    }
+
+    public function actionDelete()
+    {
+
     }
 }
--- models/AreaCode.php
+++ models/AreaCode.php
@@ -16,6 +16,19 @@ use Yii;
  */
 class AreaCode extends \app\builder\common\CommonActiveRecord
 {
+    // 状态 - 禁用
+    const STATUS_DENY = 0;
+    // 状态 - 正常
+    const STATUS_NORMAL = 1;
+
+    /**
+     * @var array
+     */
+    public static $statusMap = [
+        self::STATUS_DENY => '禁用',
+        self::STATUS_NORMAL => '正常',
+    ];
+
     /**
      * {@inheritdoc}
      */
@@ -62,5 +75,29 @@ class AreaCode extends \app\builder\common\CommonActiveRecord
             'created_at' => '创建时间',
             'updated_at' => '更新时间',
         ];
+    }
+
+    /**
+     * 获取状态标签
+     * @param int $status 状态码
+     * @param bool $isHtml 是否是`html`标签
+     * @return mixed|string
+     * @author cleverstone
+     * @since 1.0
+     */
+    public static function statusLabel($status, $isHtml = true)
+    {
+        if (!$isHtml) {
+            return self::$statusMap[$status];
+        } else {
+            switch ($status) {
+                case self::STATUS_NORMAL:  // 正常
+                    return '<span class="label label-success">正常</span>';
+                case self::STATUS_DENY:    // 禁用
+                    return '<span class="label label-danger">禁用</span>';
+                default:                   // 未知
+                    return '<span class="label label-default">未知</span>';
+            }
+        }
     }
 }
