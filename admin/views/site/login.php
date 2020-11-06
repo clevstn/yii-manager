241aa31ae1b4d27f4a390dbaa5f16f6acad4766f
cleverstone <yang_hui_lei@163.com>
2020-11-06 11:31:52 +0800

    Login Page Render

--- admin/views/site/login.php
+++ admin/views/site/login.php
@@ -14,6 +14,34 @@ SiteAsset::register($this);
 ?>
 <div class="ym-login" ng-controller="_loginCtrl" ng-cloak>
     <div class="login-wrap">
-        登录
+        <div class="panel-body">
+            <div class="login-header">
+                <img class="login-logo" src="/media/image/login-photo.jpg" alt>
+                <p class="login-title">用户登录</p>
+            </div>
+        </div>
+        <div class="panel-body">
+            <form class="p-24">
+                <div class="form-group">
+                    <label class="sr-only" for="username">username</label>
+                    <div class="input-group">
+                        <div class="input-group-addon form-group-icon">
+                            <i class="fa fa-user"></i>
+                        </div>
+                        <input type="text" class="form-control focus-define" id="username" name="username" placeholder="邮箱/用户名" autocomplete="off">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="sr-only" for="password">password</label>
+                    <div class="input-group">
+                        <div class="input-group-addon form-group-icon">
+                            <i class="fa fa-lock"></i>
+                        </div>
+                        <input type="password" class="form-control focus-define" id="password" name="password" placeholder="请输入登录密码" autocomplete="new-password">
+                    </div>
+                </div>
+                <button type="submit" class="next-step btn btn-primary">登录</button>
+            </form>
+        </div>
     </div>
 </div>
--- web/admin_static/site/login.js
+++ web/admin_static/site/login.js
@@ -9,5 +9,6 @@
     "use strict";
     _EasyApp.controller('_loginCtrl', ["$scope", "$http", "$timeout", "$interval", "$rootScope", "YmApp", "toastr", "jQuery", "yii", "YmSpinner", "Swal", "laydate", "layer", function ($scope, $http, $timeout, $interval, $rootScope, YmApp, toastr, jQuery, yii, YmSpinner, Swal, laydate, layer) {
         console.log("%c This is Login-Page with Yii-Manager", 'color:blue;');
+
     }]);
 }(window, window._EasyApp);
