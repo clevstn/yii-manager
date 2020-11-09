a3d57a04f713bf28e931fb44e79cbbd3552136ed
cleverstone <yang_hui_lei@163.com>
2020-11-09 11:19:58 +0800

    Create Record Tables

---
+++ messages/zh-CN/app.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @link http://www.cleverstone.cn/
+ * @copyright Copyright (c) 2020 黑与白
+ * @license http://yii-manager.cleverstone.cn/license/
+ */
+
+/**
+ * Message translations.
+ * NOTE: this file must be saved in UTF-8 encoding.
+ */
+
+return [
+
+];
